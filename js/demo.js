f22bae494cfcc13fc99a72cae836a3a8c1ea89f7
tail33 <295063867@qq.com>
2017-12-17 18:54:21 +0800

    修改细节

--- js/demo.js
+++ js/demo.js
@@ -1,1 +1,12 @@
-alert("hello");
+var screen=document.getElementById('screen');
+
+
+
+function step(timestamp) {
+  screen.innerHTML+=screenLeft;
+  if (true) {
+    window.requestAnimationFrame(step);
+  }
+}
+
+window.requestAnimationFrame(step);
